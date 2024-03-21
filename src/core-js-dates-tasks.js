5cfc8f7a9235a6554f998c1825ea5241cc0cecde
zyvy <gm.zyvy@gmail.com>
2024-03-21 23:12:51 +0300

    feat: did some tasks

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -129,8 +129,11 @@ function getCountDaysOnPeriod(dateStart, dateEnd) {
  * '2024-02-02', { start: '2024-02-02', end: '2024-03-02' } => true
  * '2024-02-10', { start: '2024-02-02', end: '2024-03-02' } => true
  */
-function isDateInPeriod(/* date, period */) {
-  throw new Error('Not implemented');
+function isDateInPeriod(date, period) {
+  const start = new Date(period.start);
+  const end = new Date(period.end);
+  const cur = new Date(date);
+  return cur.valueOf() <= end.valueOf() && cur.valueOf() >= start.valueOf();
 }
 
 /**
